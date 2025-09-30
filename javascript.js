ff4e69d74678dd1597ca469945519108f0240da0
ffrndzTech <199731008+ffernandezTech@users.noreply.github.com>
2025-09-30 01:07:43 -0700

    feat: added form and button in html and functions and other in javascript

---
+++ javascript.js
@@ -0,0 +1,58 @@
+const myLibrary = [];
+
+
+function Book(name, author, pages, status){
+
+    this.id = crypto.randomUUID();
+    this.name = name,
+    this.author = author,
+    this.pages = pages,
+    this.status = status,
+    this.sayInfo = function (){
+    console.log(`The book ${this.name} by ${this.author} has ${this.pages} and you have ${this.status} it`)
+
+    };
+
+}
+
+
+
+const book1 = new Book('Eragon', 'Christopher Paolini', 450, 'read');
+
+myLibrary.push(book1);
+
+
+function addBookToLibrary(name, author, pages, status){
+
+    let entry = new Book(name, author, pages, status);
+    myLibrary.push(entry);
+
+    
+
+}
+
+
+addBookToLibrary('Marsfield Park', 'Jane Austen', 500, 'reading');
+addBookToLibrary('Pride and Prejudice', 'Jane Austen', 500, 'read');
+
+
+
+
+myLibrary.forEach( item =>{
+    console.log(`ID: ${item.id}, Title: ${item.name}, Author: ${item.author} ,Number of Pages: ${item.pages},
+        Status: ${item.status} 
+        `)
+});
+
+
+const mainDisplay = document.querySelector('.mainContainer');
+const newBookBtn = document.querySelector('.CreateBook');
+
+newBookBtn.addEventListener('click', ()=>{
+
+
+
+
+});
+ 
+
